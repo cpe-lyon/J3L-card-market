71bcfc52f894c3db1c6338439318be47f7b00e45
Nogellex <leobarbier7@gmail.com>
2024-05-03 10:33:39 +0200

    initialization spring (#6)
    Co-authored-by: leo-b <leo.barbier2@etu.univ-lyon1.fr>

---
+++ atelierI/partie2/src/main/java/j3lcardmarket/atelier1/controller/RequestCrt.java
@@ -0,0 +1,38 @@
+package j3lcardmarket.atelier1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller // AND NOT @RestController
+public class RequestCrt {
+
+    @Value("${welcome.message}")
+    private String message;
+
+    private static String messageLocal="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("message", message);
+        model.addAttribute("messageLocal", messageLocal);
+
+        return "index";
+    }
+
+    @Autowired
+    CardDao cardDao;
+    @RequestMapping(value = { "/view"}, method = RequestMethod.GET)
+    public String view(Model model) {
+        model.addAttribute("myCard",cardDao.getRandomCard() );
+        return "cardView";
+    }
+
+}
+
+
