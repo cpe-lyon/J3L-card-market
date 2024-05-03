b003d73e90311c407b78a99956dee9678676b4e0
louischarnay <louis.charnay@orange.fr>
2024-05-03 09:20:06 +0200

    feat(card): :sparkles: display card from API in searchCard

--- atelierI/js/searchCard.js
+++ atelierI/js/searchCard.js
@@ -1,33 +1,41 @@
-let card = {
-            family_src:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/280px-DC_Comics_logo.png",
-            family_name:"DC comics",
-            img_src:"http://www.superherobroadband.com/app/themes/superhero/assets/img/superhero.gif",
-            name:"SUPERMAN",
-            description: "The origin story of Superman relates that he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction. Discovered and adopted by a farm couple from Kansas, the child is raised as Clark Kent and imbued with a strong moral compass. Early in his childhood, he displays various superhuman abilities, which, upon reaching maturity, he resolves to use for the benefit of humanity through a 'Superman' identity.",
-            hp: "50 HP",
-            energy:"100 Energy",
-            attack:"17 Attack",
-            defense: "80 defence"  
-        };
-let template = document.querySelector("#selectedCard");
-let clone = document.importNode(template.content, true);
+function fetchData() {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      const url = "http://tp.cpe.fr:8083/cards";
+  
+      xhr.open('GET', url, true);
+  
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.responseText)); // Résoudre la promesse avec les données
+          } else {
+            reject(new Error(xhr.statusText)); // Rejeter la promesse en cas d'erreur
+          }
+        }
+      };
+  
+      xhr.send();
+    });
+}
 
-newContent= clone.firstElementChild.innerHTML
-            .replace(/{{family_src}}/g, card.family_src)
-            .replace(/{{family_name}}/g, card.family_name)
-            .replace(/{{img_src}}/g, card.img_src)
-            .replace(/{{name}}/g, card.name)
-            .replace(/{{description}}/g, card.description)
-            .replace(/{{hp}}/g, card.hp)
-            .replace(/{{energy}}/g, card.energy)
-            .replace(/{{attack}}/g, card.attack)
-            .replace(/{{defense}}/g, card.defense);
-clone.firstElementChild.innerHTML= newContent;
+fetchData()
+  .then(cardList => {
+    let template = document.querySelector("#selectedCard");
+    let clone = document.importNode(template.content, true);
 
-let cardContainer= document.querySelector("#cardContainer");
-cardContainer.appendChild(clone);
+    newContent= clone.firstElementChild.innerHTML
+                .replace(/{{family_src}}/g, cardList[0].family_src)
+                .replace(/{{family_name}}/g, cardList[0].family)
+                .replace(/{{img_src}}/g, cardList[0].image_src)
+                .replace(/{{name}}/g, cardList[0].name)
+                .replace(/{{description}}/g, cardList[0].description)
+                .replace(/{{hp}}/g, cardList[0].hp)
+                .replace(/{{energy}}/g, cardList[0].energy)
+                .replace(/{{attack}}/g, cardList[0].attack)
+                .replace(/{{defense}}/g, cardList[0].defence);
+    clone.firstElementChild.innerHTML= newContent;
 
-
-
-
-
+    let cardContainer= document.querySelector("#cardContainer");
+    cardContainer.appendChild(clone);
+})
