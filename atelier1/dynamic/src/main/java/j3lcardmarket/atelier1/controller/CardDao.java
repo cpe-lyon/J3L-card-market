373b1f6f0c6e58b2bd6c78ce239be5b356438ccf
Josse DE OLIVEIRA <contactme@webjosse.fr>
2024-05-03 11:53:44 +0200

    atelier1: card model

--- atelier1/dynamic/src/main/java/j3lcardmarket/atelier1/controller/CardDao.java
+++ atelier1/dynamic/src/main/java/j3lcardmarket/atelier1/controller/CardDao.java
@@ -21,18 +21,7 @@ public class CardDao {
     }
 
     private void createCardList() {
-
-        Card p1=new Card("John", "pink", "super pink", "http://ekladata.com/9-cPSlYvrenNHMVawFmf_gLx8Jw.gif");
-        Card p2=new Card("Roberto", "blue", "super lazy", "http://ekladata.com/JEVyY9DkwX4vVkakeBfikSyPROA.gif");
-        Card p3=new Card("Anna", "orange", "super music girl", "http://ekladata.com/fMJl--_v-3CmisaynTHju1DMeXE.gif");
-        Card p4=new Card("Angry Joe", "purple", "super angry power", "http://ekladata.com/AmbNNNvv-4YFEMZR8XD8e54WoHc.gif");
-        Card p5=new Card("Ursula", "green", "super cloning power", "http://ekladata.com/CXJhi2YLUbNz6__e0Ct6ZP-XOds.gif");
-
-        myCardList.add(p1);
-        myCardList.add(p2);
-        myCardList.add(p3);
-        myCardList.add(p4);
-        myCardList.add(p5);
+        //base data ?
     }
     public List<Card> getCardList() {
         return this.myCardList;
