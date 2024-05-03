252f04858699abe5e324c323cc90ba02e52b53ea
Josse DE OLIVEIRA <contactme@webjosse.fr>
2024-05-03 22:09:18 +0200

    atelier1: finish card creation (post)

--- atelier1/dynamic/src/main/java/j3lcardmarket/atelier1/model/Card.java
+++ atelier1/dynamic/src/main/java/j3lcardmarket/atelier1/model/Card.java
@@ -16,6 +16,8 @@ public class Card  {
     private int price;
     private Date date;
 
+    private static int maxId = 0;
+
     public Card(String name, String description, String family, String affinity, String imgUrl, String smallImgUrl, int id, int energy, int defence, int attack, int price) {
         this.name = name;
         this.description = description;
@@ -29,53 +31,101 @@ public class Card  {
         this.attack = attack;
         this.price = price;
         this.date = new Date();
+        maxId = Math.max(maxId,id);
+    }
+    public Card(){
+        this("Card","","","","","",maxId+1,0,0,0,0);
+    }
+
+    public Date getDate() {
+        return date;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getFamily() {
         return family;
     }
 
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
     public String getAffinity() {
         return affinity;
+    }
+
+    public void setAffinity(String affinity) {
+        this.affinity = affinity;
     }
 
     public String getImgUrl() {
         return imgUrl;
     }
 
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
     public String getSmallImgUrl() {
         return smallImgUrl;
+    }
+
+    public void setSmallImgUrl(String smallImgUrl) {
+        this.smallImgUrl = smallImgUrl;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public int getEnergy() {
         return energy;
     }
 
-    public int getAttack() {
-        return attack;
+    public void setEnergy(int energy) {
+        this.energy = energy;
     }
 
     public int getDefence() {
         return defence;
     }
 
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
     public int getPrice() {
         return price;
     }
 
-    public Date getDate() {
-        return date;
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
