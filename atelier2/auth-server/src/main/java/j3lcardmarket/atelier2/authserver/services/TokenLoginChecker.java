1cc36a04890b2fb0604b7fb2dedda0070759a2ef
Webjosse.com <35874138+Webjosse@users.noreply.github.com>
2024-05-21 15:56:44 +0200

    Atelier2/login routes (#27)
    * atelier2: default app
    
    * atelier2: login routes

--- atelier2/auth-server/src/main/java/j3lcardmarket/atelier2/authserver/services/TokenLoginChecker.java
+++ atelier2/auth-server/src/main/java/j3lcardmarket/atelier2/authserver/services/TokenLoginChecker.java
@@ -2,18 +2,31 @@ package j3lcardmarket.atelier2.authserver.services;
 
 import j3lcardmarket.atelier2.authserver.models.BasicAuthInfo;
 import j3lcardmarket.atelier2.authserver.models.TokenAuthInfo;
+import j3lcardmarket.atelier2.authserver.models.TokenAuthInfoFactory;
+import j3lcardmarket.atelier2.authserver.repositories.DbLoginChecker;
+import j3lcardmarket.atelier2.commons.models.UserInfo;
 import j3lcardmarket.atelier2.commons.utils.LoginChecker;
+import j3lcardmarket.atelier2.commons.utils.UserInfoSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TokenLoginChecker implements LoginChecker<TokenAuthInfo, BasicAuthInfo> {
+    @Autowired
+    DbLoginChecker repo;
+
+    @Autowired
+    TokenAuthInfoFactory authInfoFactory;
+
     @Override
     public TokenAuthInfo checkLogin(BasicAuthInfo info) {
-        return null;
+        UserInfo uInfo = repo.checkLogin(info);
+        return authInfoFactory.createTokenAuthInfo(uInfo);
     }
 
     @Override
     public TokenAuthInfo register(BasicAuthInfo info) {
-        return null;
+        UserInfo uInfo =  repo.register(info);
+        return authInfoFactory.createTokenAuthInfo(uInfo);
     }
 }
---
+++ atelier2/auth-server/src/main/java/j3lcardmarket/atelier2/commons/utils/SignatureUtils.java
@@ -0,0 +1,47 @@
+package j3lcardmarket.atelier2.commons.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.security.*;
+import java.util.Base64;
+
+@Component
+public class SignatureUtils {
+
+    public PublicKey publicKey;
+    public PrivateKey privateKey;
+
+    public String sign(String src){
+        Signature signer = null;
+        try {
+            signer = Signature.getInstance("SHA256withRSA");
+            signer.initSign(privateKey);
+            signer.update(src.getBytes());
+            byte[] signature = signer.sign();
+            return src.replace(";", "\\;") + ";" + Base64.getEncoder().encodeToString(signature);
+        } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String unsign(String signed) throws SignatureException{
+        String[] parts = signed.split("(?<!\\\\);", 2);
+        if (parts.length != 2) throw new SignatureException("String not signed");
+        String src = parts[0].replace("\\;", ";");
+        byte[] signature = Base64.getDecoder().decode(parts[1]);
+        try {
+            Signature verifier = Signature.getInstance("SHA256withRSA");
+            verifier.initVerify(publicKey);
+            verifier.update(src.getBytes());
+            boolean isValid = verifier.verify(signature);
+            if (!isValid) throw new SignatureException("Invalid signature");
+            return src;
+        } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+}
