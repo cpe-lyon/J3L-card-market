14d9f643fda7b9c80addfdf0511b14a1d15eba92
Josse DE OLIVEIRA <contactme@webjosse.fr>
2024-05-21 13:05:43 +0200

    atelier2: default app

---
+++ atelier2/auth-server/src/main/java/j3lcardmarket/atelier2/CardApp.java
@@ -0,0 +1,13 @@
+package j3lcardmarket.atelier2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardApp.class, args);
+	}
+
+}
