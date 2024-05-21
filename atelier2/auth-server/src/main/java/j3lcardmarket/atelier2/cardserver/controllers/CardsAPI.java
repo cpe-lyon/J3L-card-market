3628cd969c04df98a8ae451df86c08aa80c553d1
Leon Dumestre <leon.dumestre@gmail.com>
2024-05-21 13:49:26 +0200

    atelier2: add card requests in api

--- atelier2/auth-server/src/main/java/j3lcardmarket/atelier2/cardserver/controllers/CardsAPI.java
+++ atelier2/auth-server/src/main/java/j3lcardmarket/atelier2/cardserver/controllers/CardsAPI.java
@@ -3,12 +3,44 @@ package j3lcardmarket.atelier2.cardserver.controllers;
 import j3lcardmarket.atelier2.cardserver.services.TransactionalCardManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CardsAPI {
     @Autowired
     TransactionalCardManager cardService;
 
-    // HTTP endpoints
+    @RequestMapping(value = {"/cards"}, method = RequestMethod.GET)
+    public String cards(Model model) {
+        model.addAttribute("cards", cardService.getAll());
+        return "cardView";
+    }
+
+    @RequestMapping(value = {"/cards/{id}"}, method = RequestMethod.GET)
+    public String card(Model model, @PathVariable Integer id) {
+        model.addAttribute("myCard", cardService.getById(id));
+        return "cardView";
+    }
+
+    @RequestMapping(value = {"/cards"}, method = RequestMethod.POST)
+    public String createCard(Model model) {
+        model.addAttribute("myCard", cardService.create());
+        return "cardView";
+    }
+
+    @RequestMapping(value = {"/cards/{id}/buy"}, method = RequestMethod.PUT)
+    public String buyCard(Model model, @PathVariable Integer id) {
+        model.addAttribute("myCard", cardService.buy(id));
+        return "cardView";
+    }
+
+    @RequestMapping(value = {"/cards/{id}/sell"}, method = RequestMethod.PUT)
+    public String sellCard(Model model, @PathVariable Integer id) {
+        model.addAttribute("myCard", cardService.sell(id));
+        return "cardView";
+    }
 
 }
--- atelier2/auth-server/src/main/java/j3lcardmarket/atelier2/cardserver/services/TransactionalCardManager.java
+++ atelier2/auth-server/src/main/java/j3lcardmarket/atelier2/cardserver/services/TransactionalCardManager.java
@@ -1,14 +1,42 @@
 package j3lcardmarket.atelier2.cardserver.services;
 
+import j3lcardmarket.atelier2.cardserver.models.Card;
 import j3lcardmarket.atelier2.cardserver.repositories.DbCardManager;
 import j3lcardmarket.atelier2.cardserver.repositories.DbTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionalCardManager {
     @Autowired
     DbCardManager cardRepo;
     @Autowired
-    DbTransactionManager TransactionRepo;
+    DbTransactionManager transactionRepo;
+
+    public List<Card> getAll() {
+        // TODO implement here
+        return null;
+    }
+
+    public Card getById(Integer id) {
+        // TODO implement here
+        return null;
+    }
+
+    public Card create() {
+        // TODO implement here
+        return null;
+    }
+
+    public Card buy(Integer id) {
+        // TODO implement here
+        return null;
+    }
+
+    public Card sell(Integer id) {
+        // TODO implement here
+        return null;
+    }
 }
