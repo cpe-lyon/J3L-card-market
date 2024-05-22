1c9bebd965eb94e324e7f02e527087cfdcd61397
louischarnay <louis.charnay@orange.fr>
2024-05-22 10:45:57 +0200

    feat: get data from API in sell page

--- atelier2/src/main/resources/static/script/sell.js
+++ atelier2/src/main/resources/static/script/sell.js
@@ -1,10 +1,35 @@
 function updateCardDetails(row) {
     const cells = row.getElementsByTagName('td');
-    document.getElementById('card-name').innerText = 'Card Name: ' + cells[0].innerText;
-    document.getElementById('card-description').innerText = 'Description: ' + cells[1].innerText;
-    document.getElementById('card-price').innerText = 'Price: ' + cells[2].innerText;
+    const price = document.getElementById('card-price').innerText;
+    document.getElementById('card-id').innerText = 'Card ID: ' + cells[0].innerText;
+    document.getElementById('card-name').innerText = 'Card Name: ' + cells[1].innerText;
     document.getElementById('sell-button').onclick = function() {
-        alert('Sold ' + cells[0].innerText + ' for ' + cells[2].innerText);
-        // Add your sell logic here
+        sellCard();
     };
 }
+
+function sellCard() {
+    const cardId = document.getElementById('card-id').innerText;
+    const price = document.getElementById('card-price').value;
+    alert('Sold ' + cardId + ' for ' + price);
+}
+
+function getCardFromAPI() {
+    fetch('http://localhost:8080/api/cards')
+        .then(response => response.json())
+        .then(data => {
+            const table = document.getElementById('card-table');
+            data.forEach(card => {
+                const row = table.insertRow();
+                row.onclick = function() {
+                    updateCardDetails(row);
+                };
+                const idCell = row.insertCell();
+                idCell.innerText = card.id;
+                const nameCell = row.insertCell();
+                nameCell.innerText = card.name;
+            });
+        });
+}
+
+getCardFromAPI();
