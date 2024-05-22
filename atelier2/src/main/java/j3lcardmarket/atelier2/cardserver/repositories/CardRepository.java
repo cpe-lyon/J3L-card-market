1ed036b3b327c03aeb98e07691ce35b870bc438f
Léon Dumestre <56174374+LeonDumestre@users.noreply.github.com>
2024-05-22 08:52:56 +0200

    atelier2: implement jpa repo (#33)

--- atelier2/src/main/java/j3lcardmarket/atelier2/cardserver/repositories/CardRepository.java
+++ atelier2/src/main/java/j3lcardmarket/atelier2/cardserver/repositories/CardRepository.java
@@ -1,26 +1,8 @@
 package j3lcardmarket.atelier2.cardserver.repositories;
 
 import j3lcardmarket.atelier2.cardserver.models.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public class CardRepository {
-
-    public List<Card> findAll() {
-        // TODO: implement
-        return null;
-    }
-
-    public Optional<Card> findById(int id) {
-        // TODO: implement
-        return null;
-    }
-
-    public Card save(Card card) {
-        // TODO: implement
-        return null;
-    }
-}
+public interface CardRepository extends JpaRepository<Card, Integer> {}
