77b3c5cfc4214dcced5e47d104e59240aed1215f
louischarnay <louis.charnay@orange.fr>
2024-05-22 14:48:41 +0200

    fix: remove unused import

--- atelier2/src/main/java/j3lcardmarket/atelier2/cardserver/dto/SellCardDto.java
+++ atelier2/src/main/java/j3lcardmarket/atelier2/cardserver/dto/SellCardDto.java
@@ -1,7 +1,6 @@
 package j3lcardmarket.atelier2.cardserver.dto;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
