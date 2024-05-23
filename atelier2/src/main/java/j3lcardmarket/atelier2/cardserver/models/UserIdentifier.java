6d3e9cfb42fcb7d39f0a321aabd498a31c76076e
Léon Dumestre <56174374+LeonDumestre@users.noreply.github.com>
2024-05-23 13:59:01 +0200

    atelier2: add balance (#57)
    * atelier2: add balance
    
    * atelier2: remove unused import

--- atelier2/src/main/java/j3lcardmarket/atelier2/cardserver/models/UserIdentifier.java
+++ atelier2/src/main/java/j3lcardmarket/atelier2/cardserver/models/UserIdentifier.java
@@ -1,6 +1,5 @@
 package j3lcardmarket.atelier2.cardserver.models;
 
-import j3lcardmarket.atelier2.cardserver.repositories.UserIdentifierRepository;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -11,8 +10,14 @@ public class UserIdentifier {
     @Id
     private String surname;
 
-    public UserIdentifier(){this(null);}
+    private Integer balance;
+
+    public UserIdentifier(){
+        this(null);
+    }
+
     public UserIdentifier(String surname){
         this.surname = surname;
+        this.balance = 100;
     }
 }
