9062f2f3d8cfa9b0c7aace659d97071bcdd434a8
Josse DE OLIVEIRA <contactme@webjosse.fr>
2024-05-27 14:28:51 +0200

    atelier3: user utils split

--- atelier3/src/main/java/j3lcardmarket/atelier3/userserver/services/UserService.java
+++ atelier3/src/main/java/j3lcardmarket/atelier3/userserver/services/UserService.java
@@ -1,18 +1,15 @@
 package j3lcardmarket.atelier3.userserver.services;
 
-import j3lcardmarket.atelier3.authserver.models.TokenAuthInfo;
-import j3lcardmarket.atelier3.commons.models.UserIdentifier;
-import j3lcardmarket.atelier3.userserver.repositories.UserIdentifierRepository;
 import j3lcardmarket.atelier3.commons.models.TimedUserInfo;
-import j3lcardmarket.atelier3.commons.utils.InvalidTokenException;
-import j3lcardmarket.atelier3.commons.utils.LoginChecker;
+import j3lcardmarket.atelier3.commons.models.UserIdentifier;
 import j3lcardmarket.atelier3.commons.utils.SignatureUtils;
 import j3lcardmarket.atelier3.commons.utils.UserInfoSerializer;
+import j3lcardmarket.atelier3.commons.utils.UserUtils;
+import j3lcardmarket.atelier3.userserver.repositories.UserIdentifierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.security.SignatureException;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -20,7 +17,7 @@ import java.util.function.Consumer;
  * In the proxy, check = register
  */
 @Service
-public class UserService implements LoginChecker<TimedUserInfo, String> {
+public class UserService extends UserUtils {
 
     @Autowired
     SignatureUtils signatureUtils;
@@ -35,38 +32,6 @@ public class UserService implements LoginChecker<TimedUserInfo, String> {
         Optional<UserIdentifier> user = repo.findById(username);
         if (user.isEmpty()) return -1;
         return user.get().getBalance();
-    }
-
-    private class TokenAuthInfoImpl implements TokenAuthInfo{
-        private final String token;
-        private final TimedUserInfo userInfo;
-
-        TokenAuthInfoImpl(String token) throws SignatureException, InvalidTokenException {
-            this.token = token;
-            String unsigned = signatureUtils.unsign(token);
-            this.userInfo = serializer.fromUnsignedToken(unsigned);
-        }
-        @Override
-        public String getToken() {
-            return token;
-        }
-        @Override
-        public TimedUserInfo getUserInfo() {
-            return userInfo;
-        }
-        @Override
-        public String serialize() {
-            return token;
-        }
-    }
-
-    @Override
-    public TimedUserInfo checkLogin(String token) {
-        try {
-            return new TokenAuthInfoImpl(token).getUserInfo();
-        } catch (SignatureException | InvalidTokenException e) {
-            return null;
-        }
     }
 
     @Cacheable(value="savedname", key="#username")
