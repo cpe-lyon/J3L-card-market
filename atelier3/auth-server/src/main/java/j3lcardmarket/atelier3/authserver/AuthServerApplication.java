9436d762743e8fb760d2ecff195aa3550097f6db
Leon Dumestre <leon.dumestre@gmail.com>
2024-05-28 16:21:50 +0200

    atelier3: implement discovery service

--- atelier3/auth-server/src/main/java/j3lcardmarket/atelier3/authserver/AuthServerApplication.java
+++ atelier3/auth-server/src/main/java/j3lcardmarket/atelier3/authserver/AuthServerApplication.java
@@ -2,9 +2,11 @@ package j3lcardmarket.atelier3.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan({"j3lcardmarket.atelier3.authserver","j3lcardmarket.atelier3.commons"})
 public class AuthServerApplication {
 
--- atelier3/game-server/src/main/java/j3lcardmarket/atelier3/gameserver/GameServerApplication.java
+++ atelier3/game-server/src/main/java/j3lcardmarket/atelier3/gameserver/GameServerApplication.java
@@ -1,4 +1,4 @@
-package j3lcardmarket.atelier3.cardserver;
+package j3lcardmarket.atelier3.gameserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan({"j3lcardmarket.atelier3.commons", "j3lcardmarket.atelier3.cardserver"})
-public class CardServerApplication {
+@ComponentScan({"j3lcardmarket.atelier3.commons", "j3lcardmarket.atelier3.gameserver"})
+public class GameServerApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.profiles.default", "cardserver");
-		SpringApplication.run(CardServerApplication.class, args);
+		System.setProperty("spring.profiles.default", "gameserver");
+		SpringApplication.run(GameServerApplication.class, args);
 	}
 
 }
