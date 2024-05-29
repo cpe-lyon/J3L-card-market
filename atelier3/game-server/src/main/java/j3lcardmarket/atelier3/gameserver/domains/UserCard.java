82ed54c171360d894bc04730a8a3886c143a84c1
Léon Dumestre <56174374+LeonDumestre@users.noreply.github.com>
2024-05-29 10:50:02 +0200

    atelier3: refactor game & add repo (#86)

--- atelier3/game-server/src/main/java/j3lcardmarket/atelier3/gameserver/domains/UserCard.java
+++ atelier3/game-server/src/main/java/j3lcardmarket/atelier3/gameserver/domains/UserCard.java
@@ -1,19 +1,24 @@
 package j3lcardmarket.atelier3.gameserver.domains;
 
+import lombok.Data;
 import lombok.Getter;
 
-@Getter
-public class UserCard extends Card {
+@Data
+public class UserCard {
 
+        private final Integer id;
+        private final String name;
         private Integer energy;
 
-        public UserCard(Integer cardId, String cardName) {
-            super(cardId, cardName);
+        public UserCard() {
+            this.id = 0;
+            this.name = "";
             this.energy = 100;
         }
 
-        public UserCard(Card card) {
-            super(card.getId(), card.getName());
+        public UserCard(Integer userCardId, String cardName) {
+            this.id = userCardId;
+            this.name = cardName;
             this.energy = 100;
         }
 
--- atelier3/game-server/src/main/java/j3lcardmarket/atelier3/gameserver/entities/GameRoomEntity.java
+++ atelier3/game-server/src/main/java/j3lcardmarket/atelier3/gameserver/entities/GameRoomEntity.java
@@ -2,16 +2,27 @@ package j3lcardmarket.atelier3.gameserver.entities;
 
 import j3lcardmarket.atelier3.gameserver.domains.GameRoomState;
 import jakarta.persistence.*;
-import org.springframework.context.annotation.Primary;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "game_room")
 public class GameRoomEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
+
     private GameRoomState state;
+
     private String creator;
+
+    @OneToOne
+    private PlayerCardEntity creatorCard;
+
     private String opponent;
+
+    @OneToOne
+    private PlayerCardEntity opponentCard;
 }
